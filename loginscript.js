e8b8cf4054fd85dbe368625b70db4cd7c923c4cc
DEERAJ <158032353+AthipatlaDeeraj@users.noreply.github.com>
2024-02-21 21:34:44 +0530

    Add files via upload
    doing my first website

---
+++ loginscript.js
@@ -0,0 +1,9 @@
+function myfn(){
+    let passwo=document.getElementById("pass").value;
+    if(passwo=="Deeraj"){
+        alert("password Correct good to go ahead");
+    }
+    else{alert("password incorrect");}
+}
+const d=new Date("2024-02-20");
+document.getElementById("dat")=d;
