9ccd4b3f176b414bb3e952eef7525457f8dd73cd
DEERAJ <158032353+AthipatlaDeeraj@users.noreply.github.com>
2024-03-03 12:15:49 +0530

    loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -1,9 +1,14 @@
-function myfn(){
-    let passwo=document.getElementById("pass").value;
-    if(passwo=="Deeraj"){
-        alert("password Correct good to go ahead");
-    }
-    else{alert("password incorrect");}
-}
-const d=new Date("2024-02-20");
-document.getElementById("dat")=d;
+let pass=document.querySelector("#pass");
+let fn=document.querySelector("#fname");
+let ln=document.querySelector("#lname");
+let form=document.querySelector("form");
+
+form=addEventListener("submit",(event)=>{
+        event.preventDefault();
+        if(pass.value=="Deeru"&&fn.value=="athipatla"&&ln.value=="deerajkumar"){
+            window.location.href="homepage.html";
+        }
+        else{
+            alert("wrong password");
+        }
+});
